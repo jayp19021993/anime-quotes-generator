d8430fe08aaebd781c52167b8bd5416161891caa
Jay Patel <jay_patel_123@hotmail.co.uk>
2023-08-18 13:15:24 +0100

    anime quotes generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from "react";
+import Quote from "./components/Quote.js"; 
 
 function App() {
+  const[quote, setQuote]= useState({
+    anime:null, 
+    character:null, 
+    quote:null
+  })
+
+  const fetchQuote= async()=>{
+    return await fetch('https://animechan.xyz/api/random')
+    .then(response => response.json());
+  } 
+
+  const Generator = async()=>{
+    setQuote(await fetchQuote());
+  }
+
+  useEffect(async()=>{
+    setQuote(await fetchQuote());
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Quote/> 
+
+      <button onClick={Generator}>Generator new quote</button>
     </div>
   );
 }
